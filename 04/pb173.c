3ee95255f1744feb16a5e4b032d57acbf4e307a8
Jiri Slaby <jirislaby@gmail.com>
2017-03-09 10:45:15 +0100

    04: more skeleton
    Signed-off-by: Jiri Slaby <jirislaby@gmail.com>

--- 04/pb173.c
+++ 04/pb173.c
@@ -13,25 +13,53 @@ static ssize_t my_read(struct file *filp, char __user *buf, size_t count,
 	return simple_read_from_buffer(buf, count, off, string, strlen(string));
 }
 
-static struct file_operations my_fops = {
+static struct file_operations my_rfops = {
 	.owner = THIS_MODULE,
 	.read = my_read,
 };
 
-static struct miscdevice my_misc = {
+static struct miscdevice my_rmisc = {
 	.minor = MISC_DYNAMIC_MINOR,
-	.fops = &my_fops,
-	.name = "my_name",
+	.fops = &my_rfops,
+	.name = "my_read",
+};
+
+static ssize_t my_write(struct file *filp, const char __user *buf, size_t count,
+		loff_t *off)
+{
+	return count;
+}
+
+static struct file_operations my_wfops = {
+	.owner = THIS_MODULE,
+	.write = my_write,
+};
+
+static struct miscdevice my_wmisc = {
+	.minor = MISC_DYNAMIC_MINOR,
+	.fops = &my_wfops,
+	.name = "my_write",
 };
 
 static int my_init(void)
 {
-	return misc_register(&my_misc);
+	int ret = misc_register(&my_rmisc);
+	if (ret)
+		return ret;
+
+	ret = misc_register(&my_wmisc);
+	if (ret) {
+		misc_deregister(&my_rmisc);
+		return ret;
+	}
+
+	return 0;
 }
 
 static void my_exit(void)
 {
-	misc_deregister(&my_misc);
+	misc_deregister(&my_rmisc);
+	misc_deregister(&my_wmisc);
 }
 
 module_init(my_init);
